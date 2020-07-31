0c6bd6a356459908b9570a2968fc7e2c4cfe7f1d
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-08-01 00:57:38 +0200

    simplify ProveNamespace by keeping track of min/max namespace and improve doc

--- nmt.go
+++ nmt.go
@@ -1,6 +1,7 @@
 package nmt
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 
@@ -26,11 +27,14 @@ type NamespacedMerkleTree struct {
 	// this can be used to efficiently lookup the range for an
 	// existing namespace without iterating through the leafs
 	namespaceRanges map[string]merkletree.LeafRange
+	minNID          namespace.ID
+	maxNID          namespace.ID
 }
 
 func New(treeHasher treehasher.NmTreeHasher) *NamespacedMerkleTree {
 	return &NamespacedMerkleTree{
 		treeHasher: treeHasher,
+		tree:       merkletree.NewFromTreehasher(treeHasher),
 		// XXX: 100 seems like a good capacity for the leafs slice
 		// but maybe this should also be a constructor param: for cases the caller
 		// knows exactly how many leafs will be pushed this will save allocations
@@ -39,7 +43,8 @@ func New(treeHasher treehasher.NmTreeHasher) *NamespacedMerkleTree {
 		leafs:           make([]namespace.PrefixedData, 0, 100),
 		leafHashes:      make([][]byte, 0, 100),
 		namespaceRanges: make(map[string]merkletree.LeafRange),
-		tree:            merkletree.NewFromTreehasher(treeHasher),
+		minNID:          bytes.Repeat([]byte{0xFF}, treeHasher.NamespaceSize()),
+		maxNID:          bytes.Repeat([]byte{0x00}, treeHasher.NamespaceSize()),
 	}
 }
 
@@ -61,15 +66,19 @@ func (n NamespacedMerkleTree) Prove(index int) (
 	return
 }
 
-// ProveNamespace returns some kind of range proof for the given NamespaceID.
-// In case the underlying tree contains leafs with the given namespace
-// their start and end index will be returned together with a range proof and the found leafs.
-// In the above case the leafHashes will be nil.
-// If the tree does not have any entries with the given NamespaceID,
-// but the will be proven by returning the (namespaced or rather flagged)
-// hashes of the leafs that would be in the range if they existed.
-// Either foundLeafs or leafHashes should be nil.
-// Note that in cases (nID < minNID) or (maxNID < nID) we do not
+// ProveNamespace returns a range proof for the given NamespaceID.
+//
+//In case the underlying tree contains leafs with the given namespace
+// their start and end index will be returned together with a range proof and
+// the found leafs. In that case the returned leafHashes will be nil.
+//
+// If the tree does not have any entries with the given Namespace ID,
+// but the namespace is within the range of the tree's min and max namespace,
+// this will be proven by returning the (namespaced or rather flagged)
+// hashes of the leafs that would be in that range if they existed. In that
+// case the returned leafs will be nil.
+//
+// In the case (nID < minNID) or (maxNID < nID) we do not
 // generate any proof and we return an empty range (0,0) to
 // indicate that this namespace is not contained in the tree.
 func (n NamespacedMerkleTree) ProveNamespace(nID namespace.ID) (
@@ -79,69 +88,78 @@ func (n NamespacedMerkleTree) ProveNamespace(nID namespace.ID) (
 	foundLeafs []namespace.PrefixedData,
 	leafHashes [][]byte, // XXX: introduce a type/type alias, e.g FlaggedHas
 ) {
-	found, proofStart, proofEnd := n.foundNamespaceID(nID)
+	// In the cases (nID < minNID) or (maxNID < nID),
+	// return empty range and no proof:
+	if nID.Less(n.minNID) || n.maxNID.Less(nID) {
+		return 0, 0, nil, nil, nil
+	}
 
-	// If we did not find the namespace, that either means that there is a gap in the tree
-	// i.e. a range where this namespace would live if it was pushed,
-	// or, it is smaller or greater than any pushed namespace.
-	// XXX this can probably be simplified, too:
-	foundRangeStart := false
+	found, proofStart, proofEnd := n.foundInRange(nID)
 	if !found {
-		// Generate a proof for an absence using the
-		// range the namespace would be in
-		// TODO: document this as a proper godoc comment
-		var prevLeaf namespace.PrefixedData
-		for index, curLeaf := range n.leafs {
-			if index == 0 {
-				prevLeaf = curLeaf
-				continue
-			}
-			prevNs := prevLeaf.NamespaceID()
-			currentNs := curLeaf.NamespaceID()
-			// Note that here we would also care for the case
-			// current < nId < prevNs
-			// but we only allow pushing leafs with ascending namespaces;
-			// i.e. prevNs <= currentNs is always true.
-			// Also we only check for strictly smaller: prev < nid < current
-			// because if we either side was equal, we would have found the
-			// namespace before.
-			if prevNs.Less(nID) && nID.Less(currentNs) {
-				if !foundRangeStart {
-					foundRangeStart = true
-					proofStart = index
-				}
-				proofEnd = index + 1
-			}
-			prevLeaf = curLeaf
-		}
+		// To generate a proof for an absence we calculate the
+		// range the namespace would be in:
+		proofStart, proofEnd = n.calculateAbsenceRange(nID)
 	}
-	if found || foundRangeStart {
-		subTreeHasher := internal.NewCachedSubtreeHasher(n.leafHashes, n.treeHasher)
-		var err error
-		proof, err = merkletree.BuildRangeProof(proofStart, proofEnd, subTreeHasher)
-		if err != nil {
-			// This should never happen.
-			// TODO would be good to back this by more tests and fuzzing.
-			panic(fmt.Sprintf(
-				"unexpected err: %v on nID: %v, range: [%v, %v)",
-				err,
-				nID,
-				proofStart,
-				proofEnd,
-			))
-		}
+	// At this point we either found the namespace in the tree or calculated
+	// the range it would be in (to generate a proof of absence and to return
+	// the corresponding leaf hashes).
+	subTreeHasher := internal.NewCachedSubtreeHasher(n.leafHashes, n.treeHasher)
+	var err error
+	proof, err = merkletree.BuildRangeProof(proofStart, proofEnd, subTreeHasher)
+	if err != nil {
+		// This should never happen.
+		// TODO would be good to back this by more tests and fuzzing.
+		panic(fmt.Sprintf(
+			"unexpected err: %v on nID: %v, range: [%v, %v)",
+			err,
+			nID,
+			proofStart,
+			proofEnd,
+		))
 	}
+
 	proofMessages := n.leafs[proofStart:proofEnd]
 	if found {
+		// Return (inclusion) range proof:
 		return proofStart, proofEnd, proof, proofMessages, nil
 	}
-	// Note that in cases (nID < minNID) or (maxNID < nID) we do not generate any
-	// proof. Also we return an empty range (0,0) to indicate that this namespace is
-	// *not* contained in the tree.
+	// Return proof of absence (returning the leaf hashes:
 	return proofStart, proofEnd, proof, nil, n.leafHashes[proofStart:proofEnd]
 }
 
-func (n *NamespacedMerkleTree) foundNamespaceID(nID namespace.ID) (bool, int, int) {
+func (n NamespacedMerkleTree) calculateAbsenceRange(nID namespace.ID) (int, int) {
+	foundRangeStart := false
+	proofStart, proofEnd := 0, 0
+	var prevLeaf namespace.PrefixedData
+	for index, curLeaf := range n.leafs {
+		if index == 0 {
+			prevLeaf = curLeaf
+			continue
+		}
+		prevNs := prevLeaf.NamespaceID()
+		currentNs := curLeaf.NamespaceID()
+		// Note that here we would also care for the case
+		// current < nId < prevNs
+		// but we only allow pushing leafs with ascending namespaces;
+		// i.e. prevNs <= currentNs is always true.
+		// Also we only check for strictly smaller: prev < nid < current
+		// because if we either side was equal, we would have found the
+		// namespace before.
+		if prevNs.Less(nID) && nID.Less(currentNs) {
+			if !foundRangeStart {
+				foundRangeStart = true
+				proofStart = index
+			}
+			proofEnd = index + 1
+		}
+		prevLeaf = curLeaf
+	}
+	return proofStart, proofEnd
+}
+
+func (n *NamespacedMerkleTree) foundInRange(nID namespace.ID) (bool, int, int) {
+	// This is a faster version of this code snippet:
+	// https://github.com/lazyledger/lazyledger-prototype/blob/2aeca6f55ad389b9d68034a0a7038f80a8d2982e/simpleblock.go#L106-L117
 	foundRng, found := n.namespaceRanges[string(nID)]
 	// XXX casting from uint64 to int is kinda crappy but nebolousLabs'
 	// range proof api requires int params only to convert them to uint64 ...
@@ -176,9 +194,16 @@ func (n *NamespacedMerkleTree) Push(data namespace.PrefixedData) error {
 		}
 	}
 	n.tree.Push(data.Bytes())
+	// update relevant "caches":
 	n.leafs = append(n.leafs, data)
 	n.leafHashes = append(n.leafHashes, n.treeHasher.HashLeaf(data.Bytes()))
 	n.updateNamespaceRanges()
+	if data.NamespaceID().Less(n.minNID) {
+		n.minNID = data.NamespaceID()
+	}
+	if n.maxNID.Less(data.NamespaceID()) {
+		n.maxNID = data.NamespaceID()
+	}
 	return nil
 }
 
