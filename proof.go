e679661c6776d8a694f4f7c423c2e2eccb6c5aaa
Evan Forbes <42654277+evan-forbes@users.noreply.github.com>
2022-05-30 18:09:34 -0500

    Verify inclusion proofs with multiple leaves (#58)
    * support multiple leaves while proving
    
    * add test and debug
    
    * format appends
    
    * add comment to VerifyInclusion
    
    * add more docs
    
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>
    
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>

--- proof.go
+++ proof.go
@@ -206,10 +206,19 @@ func (proof Proof) verifyLeafHashes(nth *Hasher, verifyCompleteness bool, nID na
 	return bytes.Equal(tree.Root(), root)
 }
 
-func (proof Proof) VerifyInclusion(h hash.Hash, nid namespace.ID, data []byte, root []byte) bool {
+// VerifyInclusion checks that the inclusion proof is valid by using leaf data
+// and the provided proof to regenerate and compare the root. Note that the leaf
+// data should not contain the prefixed namespace, unlike the tree.Push method,
+// which takes prefixed data. All leaves implicitly have the same namespace ID: `nid`.
+func (proof Proof) VerifyInclusion(h hash.Hash, nid namespace.ID, leaves [][]byte, root []byte) bool {
 	nth := NewNmtHasher(h, nid.Size(), proof.isMaxNamespaceIDIgnored)
-	leafData := append(nid, data...)
-	return proof.verifyLeafHashes(nth, false, nid, [][]byte{nth.HashLeaf(leafData)}, root)
+	hashes := make([][]byte, len(leaves))
+	for i, d := range leaves {
+		leafData := append(append(make([]byte, 0, len(d)+len(nid)), nid...), d...)
+		hashes[i] = nth.HashLeaf(leafData)
+	}
+
+	return proof.verifyLeafHashes(nth, false, nid, hashes, root)
 }
 
 // nextSubtreeSize returns the size of the subtree adjacent to start that does
