f4504d1e683abc735b3d4c0c5d5e02565dce37e6
sanaz <sanaztaheri@ieee.org>
2023-02-08 12:44:44 -0800

    adds documentation for id

--- namespace/id.go
+++ namespace/id.go
@@ -4,22 +4,27 @@ import "bytes"
 
 type ID []byte
 
+// Less returns true if nid < ID, otherwise, false
 func (nid ID) Less(other ID) bool {
 	return bytes.Compare(nid, other) < 0
 }
 
+// Equal returns true if nid == ID, otherwise, false
 func (nid ID) Equal(other ID) bool {
 	return bytes.Equal(nid, other)
 }
 
+// LessOrEqual returns true if nid <= ID, otherwise, false
 func (nid ID) LessOrEqual(other ID) bool {
 	return bytes.Compare(nid, other) <= 0
 }
 
+// Size returns the byte size of the ID
 func (nid ID) Size() IDSize {
 	return IDSize(len(nid))
 }
 
+// String stringifies the ID
 func (nid ID) String() string {
 	return string(nid)
 }
