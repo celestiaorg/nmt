72a99b40af6cc6d3075201244c692a24626d791f
sanaz <sanaztaheri@ieee.org>
2023-02-16 10:39:47 -0800

    picks more clear variable names

--- hasher.go
+++ hasher.go
@@ -198,9 +198,9 @@ func (n *Hasher) HashNode(left, right []byte) []byte {
 	rightMinNs, rightMaxNs := right[:n.NamespaceLen], right[n.NamespaceLen:flagLen]
 
 	// check the namespace range of the left and right children
-	nIDRMin := namespace.ID(rightMinNs)
-	nIDLMax := namespace.ID(leftMaxNs)
-	if nIDRMin.Less(nIDLMax) {
+	RMinNID := namespace.ID(rightMinNs)
+	LMaxNID := namespace.ID(leftMaxNs)
+	if RMinNID.Less(LMaxNID) {
 		panic("nodes are out of order: the maximum namespace of the left child is greater than the min namespace of the right child")
 	}
 
